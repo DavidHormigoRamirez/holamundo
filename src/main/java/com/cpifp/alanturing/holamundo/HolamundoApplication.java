0b2ff8684698a044fbaa79cf634aa134a62f67c2
DavidHormigoRamirez <115550973+DavidHormigoRamirez@users.noreply.github.com>
2024-04-30 19:10:24 +0200

    Update HolamundoApplication.java

--- src/main/java/com/cpifp/alanturing/holamundo/HolamundoApplication.java
+++ src/main/java/com/cpifp/alanturing/holamundo/HolamundoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HolamundoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HolamundoApplication.class, args);
+		SpringApplication.run(HolamundoApplication.class, args
 	}
 
 }
